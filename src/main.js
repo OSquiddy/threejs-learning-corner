f8112531e0f7f99adda563edaf4560e77d5f28ac
OSquiddy <siddiqui.omar21@gmail.com>
2025-07-08 08:23:56 -0500

    First 3JS commit. Created a 3D cube. Learned the basics of scenes, cameras and renderers.

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+const scene = new THREE.Scene()
+
+const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red' })
+
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+
+scene.add(cubeMesh)
+
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 30)
+
+camera.position.z = 5
+// scene.add(camera)
+
+
+const canvas = document.querySelector('.threejs')
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.autoRotate = true
+
+
+const renderLoop = () => {
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(renderLoop)
+}
+
+renderLoop()
